6db15ce18f348f5a143ba3cf45dbb15fc3f6e455
Jackson <jackson.bounds@gmail.com>
2022-06-03 08:09:38 -0400

    Can now add playlist using a YouTube url w/ !play

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import asyncio
 import math
 import os
 import re
-
 import aiohttp
 import discord
 import random
@@ -13,7 +12,6 @@ import pytube
 import pytube.exceptions
 import tempfile
 import shutil
-from contextlib import contextmanager
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
@@ -23,7 +21,8 @@ musicCtx = None
 class MusicPlayer:
     def __init__(self, ctx):
         self.MAX_VIDEO_LENGTH = 600  # in seconds -> 10 minutes
-        self.current = {'filename': None, 'video': None, 'source': None, 'sendGif': False}
+        self.current = {'filename': None, 'video': None, 'sendGif': False}
+        self.source = None
         self.currentStartTime = time.time()
         self.pauseStart = 0
         self.currentPauseTime = 0
@@ -50,8 +49,29 @@ class MusicPlayer:
             except (pytube.exceptions.RegexMatchError or pytube.exceptions.VideoPrivate or
                     pytube.exceptions.VideoUnavailable):
                 # TODO probably need to catch more errors
-                await self.ctx.message.reply('**Given link returned no results.**')
-                raise commands.CommandError('HANDLED')
+                # Check if the query is a playlist
+                if 'playlist' in query:
+                    try:
+                        playlist = pytube.Playlist(query)
+                        for video in playlist.videos:
+                            filename_ = "".join(
+                                [c for c in video.title if c.isalpha() or c.isdigit() or c == ' ']).rstrip()
+                            filename_ = re.sub(' +', ' ', filename_)
+                            filename_ += '.mp4'
+                            print("added {}".format(filename_))
+                            self.queue.append(
+                                {'filename': filename_, 'video': video, 'sendGif': sendGif})
+                        await self.checkQueue()
+                        return
+                    except (pytube.exceptions.RegexMatchError or pytube.exceptions.VideoPrivate or
+                            pytube.exceptions.VideoUnavailable):
+                        # Query was a playlist, but didn't return any results (likely incorrect URL)
+                        await self.ctx.message.reply('Given playlist link returned no results.')
+                        raise commands.CommandError('HANDLED')
+                else:
+                    # The query wasn't a playlist and the link returned no result
+                    await self.ctx.message.reply('Given link returned no results.')
+                    raise commands.CommandError('HANDLED')
         else:
             # Search YouTube for the video
             search = pytube.Search(query)
@@ -59,68 +79,79 @@ class MusicPlayer:
             while True:
                 try:
                     video = search.results[i]
-                    if video.length > self.MAX_VIDEO_LENGTH:
+                    if (video.length > self.MAX_VIDEO_LENGTH) or (video.length == 0):
                         i += 1
                         continue
-                    stream = pytube.YouTube(video.watch_url)
-                    toDownload = stream.streams.filter(only_audio=True)
                     break
-                except pytube.exceptions.LiveStreamError:
-                    # Video is a live stream
-                    i += 1
                 except IndexError:
                     # Ran out of search results
                     await self.ctx.message.reply('No results were found.')
                     raise commands.CommandError('HANDLED')
-        assert toDownload is not None
 
         filename_ = "".join([c for c in video.title if c.isalpha() or c.isdigit() or c == ' ']).rstrip()
         filename_ = re.sub(' +', ' ', filename_)
         filename_ += '.mp4'
         await self.ctx.message.reply('Added to queue: `' + video.title + '`')
-        toDownload[0].download(self.songsDir, filename=filename_)
-        source = discord.FFmpegPCMAudio(self.songsDir + '/' + filename_)
-        self.queue.append({'filename': filename_, 'video': video, 'source': source, 'sendGif': sendGif})
-        await self.checkQueue(None)
+        self.queue.append(
+            {'filename': filename_, 'video': video, 'sendGif': sendGif})
+        await self.checkQueue()
 
-    async def checkQueue(self, sourceToClean):
-        if sourceToClean is not None:
-            sourceToClean.cleanup()
+    async def checkQueue(self):
+        if (self.source is not None) and (not self.player.is_playing()) and (not self.player.is_paused()):
+            # Clean up after song end
+            self.source.cleanup()
             current_ = self.songsDir + '/' + self.current['filename']
             os.remove(current_)
-            self.current['source'] = None
-        if self.player.is_playing() or self.player.is_paused():
-            return
+            self.source = None
         try:
             self.current = self.queue.pop(0)
             self.pauseStart = 0
             self.currentPauseTime = 0
         except IndexError:
+            self.source = None
             return await self.destroy()
         await self.playAudio()
 
     async def playAudio(self):
-        source = self.current['source']
-        assert source is not None
+        video = self.current['video']
+        stream = pytube.YouTube(video.watch_url)
+        toDownload = stream.streams.filter(only_audio=True)
+        fileName = self.current['filename']
+        toDownload[0].download(self.songsDir, filename=fileName)
+        source = discord.FFmpegPCMAudio(self.songsDir + '/' + fileName)
+
+        self.source = source
         self.currentStartTime = time.time()
-        self.player.play(source, after=lambda x=source: asyncio.run_coroutine_threadsafe(
-            self.checkQueue(x), bot.loop))
-        msg = await self.ctx.send('>>> Now playing: `' + self.current['video'].title + '`')
+        # TODO Error with positional args here
+        self.player.play(source, after=lambda: asyncio.run_coroutine_threadsafe(
+            self.checkQueue(), bot.loop))
+        await self.ctx.send('>>> Now playing: `' + video.title + '`')
 
         if self.current['sendGif'] is True:
             # Send a gif with 'Now playing: '
-            await giphySendGif(self.ctx, self.current['video'].title)
+            await giphySendGif(self.ctx, video.title)
 
     async def showQueue(self):
         video = self.current['video']
         assert video is not None
-        queueString = '```Current: {} | ({} / {})\n\n'.format(video.title, self.getCurrentTimestamp(),
+        isAre = 'are'
+        sAdd = 's'
+        if len(self.queue) == 1:
+            isAre = 'is'
+            sAdd = ''
+        queueString = 'There {} **{}** song{} in the queue.\n'.format(isAre, len(self.queue), sAdd)
+        queueString += '```Current: {} | ({} / {})\n\n'.format(video.title, self.getCurrentTimestamp(),
                                                               MusicPlayer.getVideoLength(video))
         i = 1
         for element in self.queue:
             video = element['video']
-            queueString += '{}: {} | ({})\n'.format(i, video.title, MusicPlayer.getVideoLength(video))
+            toAdd = '{}: {} | ({})\n'.format(i, video.title, MusicPlayer.getVideoLength(video))
             i += 1
+            if (len(toAdd) + len(queueString)) > 1950:
+                # String is going to be too long with this addition
+                queueString += '> and {} more...'.format(len(self.queue) + 2 - i)
+                break
+            queueString += toAdd
         queueString += '```'
         await self.ctx.message.reply(queueString)
 
@@ -141,7 +172,7 @@ class MusicPlayer:
 
     async def skip(self):
         self.player.stop()
-        await self.checkQueue(self.current['source'])
+        await self.checkQueue()
 
     def getCurrentTimestamp(self):
         endTime = time.time()
@@ -150,6 +181,11 @@ class MusicPlayer:
         else:
             timeInSeconds = (endTime - self.currentStartTime) - (endTime - self.pauseStart) - self.currentPauseTime
         return MusicPlayer.convertToTimeStamp(timeInSeconds)
+
+    def shuffle(self):
+        random.shuffle(self.queue)
+
+    #async def
 
     @staticmethod
     def getVideoLength(video):
@@ -165,7 +201,7 @@ class MusicPlayer:
 
     @staticmethod
     def isURL(query):
-        if '.com' or 'http' in query:
+        if '.com' in query or 'http' in query:
             return True
         else:
             return False
@@ -179,7 +215,7 @@ class MusicPlayer:
         if self.player.is_playing():
             self.player.stop()
         if os.path.exists(self.songsDir):
-            while self.current['source'] is not None:
+            while self.source is not None:
                 # Need to ensure that the current source is removed before deleting the file
                 time.sleep(.25)
             shutil.rmtree(self.songsDir)
@@ -196,25 +232,26 @@ async def giphySendGif(ctx, query):
     # This needs to include 'query' since ctx.message.content may not be what we need to search (MusicPlayer)
     embed = discord.Embed(colour=discord.Colour.purple())
     session = aiohttp.ClientSession()
-
     query.replace(' ', '+')
+    query = query[:50]  # Max length of a search query is 50 characters
     response = await session.get(
         'http://api.giphy.com/v1/gifs/search?q=' + query + '&api_key=' + os.getenv('GIPHY_API_KEY') + '&limit=10')
     data = json.loads(await response.text())
     gifChoice = random.randint(0, 9)
     embed.set_image(url=data['data'][gifChoice]['images']['original']['url'])
     await session.close()
+    if response.ok is False:
+        return
     await ctx.channel.send(embed=embed)
+
 @bot.after_invoke
 async def reactOnSuccess(ctx):
     if not ctx.command_failed:
         await ctx.message.add_reaction('✅')
 
-
 @bot.event
 async def on_ready():
     print('Logged in as {0.user}'.format(bot))
-
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -237,20 +274,17 @@ async def on_command_error(ctx, error):
 async def echo(ctx, *, arg):
     await ctx.message.reply(arg)
 
-
 @bot.command()
 async def add(ctx, a: int, b: int):  # converts a and b to ints during invoke
     if a == 9 and b == 10:
         a = 11  # 9 + 10 = 21
     await ctx.message.reply(a + b)
 
-
 @bot.command()
 async def whereAmI(ctx):
     if getUserVoiceChannel(ctx) is None:
         return await ctx.message.reply('You are not in a voice channel currently.')
     await ctx.message.reply(getUserVoiceChannel(ctx))
-
 
 @bot.command()
 async def join(ctx):
@@ -262,7 +296,6 @@ async def join(ctx):
         return await ctx.voice_client.move_to(voiceChannel)
     await voiceChannel.connect()
 
-
 @bot.command()
 async def leave(ctx):
     if musicPlayer is None:
@@ -270,6 +303,10 @@ async def leave(ctx):
     else:
         await musicPlayer.destroy()
 
+# Copy of 'leave'
+@bot.command()
+async def quit(ctx):
+    await ctx.invoke(bot.get_command('leave'))
 
 @bot.command()
 async def play(ctx, sendGif_):
@@ -286,11 +323,19 @@ async def play(ctx, sendGif_):
     musicPlayer.updateCtx(ctx)
     await musicPlayer.addToQueue(sendGif_)
 
+# Copy of 'play'
+@bot.command()
+async def p(ctx):
+    await ctx.invoke(bot.get_command('play'), False)
+
 @bot.command()
 async def playi(ctx):
     await ctx.invoke(bot.get_command('play'), True)
 
-
+# Copy of 'playi'
+@bot.command()
+async def pi(ctx):
+    await ctx.invoke(bot.get_command('play'), True)
 
 @bot.command()
 async def queue(ctx):
@@ -300,6 +345,10 @@ async def queue(ctx):
     musicPlayer.updateCtx(ctx)
     await musicPlayer.showQueue()
 
+# Copy of 'queue'
+@bot.command()
+async def q(ctx):
+    await ctx.invoke(bot.get_command('queue'))
 
 @bot.command()
 async def pause(ctx):
@@ -308,7 +357,6 @@ async def pause(ctx):
         return
     musicPlayer.pause()
 
-
 @bot.command()
 async def resume(ctx):
     if musicPlayer is None:
@@ -316,6 +364,10 @@ async def resume(ctx):
         return
     musicPlayer.resume()
 
+# Copy of 'resume'
+@bot.command()
+async def unpause(ctx):
+    await ctx.invoke(bot.get_command('resume'))
 
 @bot.command()
 async def skip(ctx):
@@ -323,6 +375,18 @@ async def skip(ctx):
         await ctx.message.reply('There is nothing to skip.')
         return
     await musicPlayer.skip()
+
+# Copy of 'skip'
+@bot.command()
+async def s(ctx):
+    await ctx.invoke(bot.get_command('skip'))
+
+@bot.command()
+async def shuffle(ctx):
+    if musicPlayer is None:
+        await ctx.message.reply('There is no queue.')
+        return
+    musicPlayer.shuffle()
 
 
 @bot.command()
@@ -332,11 +396,12 @@ async def randomMsg(ctx):
     await selected.reply('This message is from ---')
     # TODO overhaul this
 
-
 @bot.command()
 async def fox(ctx):
     response = requests.get('https://randomfox.ca/floof').json()
     await ctx.message.reply(response.get('image'))
+
+
 
 
 bot.run(os.getenv('DISCORD_TOKEN'))
