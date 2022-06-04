5944b7896a7789c41cb9d6a11b7513a15f1ec6cd
Jackson <jackson.bounds@gmail.com>
2022-06-04 09:16:53 -0400

    Multi-threading and semaphores added to playlist capability. Now doesn't need to wait for the entire playlist to be added before playing.

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ import pytube
 import pytube.exceptions
 import tempfile
 import shutil
+import threading
+import concurrent.futures
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
@@ -30,6 +32,9 @@ class MusicPlayer:
         self.ctx = ctx
         self.player = self.ctx.voice_client
         self.songsDir = tempfile.mkdtemp()
+        self.queueLock = threading.Lock()
+        self.queueSemaphore = threading.Semaphore(0)
+
 
     def updateCtx(self, ctx):
         self.ctx = ctx
@@ -37,37 +42,22 @@ class MusicPlayer:
     async def addToQueue(self, sendGif):
         if self.ctx.message.content is None:
             raise commands.CommandError('No media was specified.')
+
+        songsToAdd = 1
+        playlist = None
         query = self.ctx.message.content.split(' ', 1)[1]
 
-        toDownload = None
         if MusicPlayer.isURL(query):
             # This is a link to a video
             try:
                 video = pytube.YouTube(query)
-                stream = pytube.YouTube(video.watch_url)
-                toDownload = stream.streams.filter(only_audio=True)
             except (pytube.exceptions.RegexMatchError or pytube.exceptions.VideoPrivate or
                     pytube.exceptions.VideoUnavailable):
                 # TODO probably need to catch more errors
                 # Check if the query is a playlist
                 if 'playlist' in query:
-                    try:
-                        playlist = pytube.Playlist(query)
-                        for video in playlist.videos:
-                            filename_ = "".join(
-                                [c for c in video.title if c.isalpha() or c.isdigit() or c == ' ']).rstrip()
-                            filename_ = re.sub(' +', ' ', filename_)
-                            filename_ += '.mp4'
-                            print("added {}".format(filename_))
-                            self.queue.append(
-                                {'filename': filename_, 'video': video, 'sendGif': sendGif})
-                        await self.checkQueue()
-                        return
-                    except (pytube.exceptions.RegexMatchError or pytube.exceptions.VideoPrivate or
-                            pytube.exceptions.VideoUnavailable):
-                        # Query was a playlist, but didn't return any results (likely incorrect URL)
-                        await self.ctx.message.reply('Given playlist link returned no results.')
-                        raise commands.CommandError('HANDLED')
+                    playlist = pytube.Playlist(query)
+                    songsToAdd = len(playlist)
                 else:
                     # The query wasn't a playlist and the link returned no result
                     await self.ctx.message.reply('Given link returned no results.')
@@ -88,13 +78,59 @@ class MusicPlayer:
                     await self.ctx.message.reply('No results were found.')
                     raise commands.CommandError('HANDLED')
 
-        filename_ = "".join([c for c in video.title if c.isalpha() or c.isdigit() or c == ' ']).rstrip()
-        filename_ = re.sub(' +', ' ', filename_)
-        filename_ += '.mp4'
-        await self.ctx.message.reply('Added to queue: `' + video.title + '`')
-        self.queue.append(
-            {'filename': filename_, 'video': video, 'sendGif': sendGif})
-        await self.checkQueue()
+        # If we get here, there is/are valid song(s) to queue
+        if songsToAdd == 1:
+            ret = self.thrAddQueue(video, sendGif)
+            if ret is True:
+                await self.ctx.message.reply('Added to queue: `' + video.title + '`')
+                await self.checkQueue()
+            else:
+                # This should NOT happen, but if in any case it does, this will throw an error
+                raise Exception
+        else:
+            # Adding multiple songs (playlist)
+            await self.ctx.message.reply('Attempting to add **{}** songs to the queue'.format(len(playlist)))
+            thr = threading.Thread(target=self.createThreads, daemon=True, args=(playlist, sendGif))
+            thr.start()
+            self.queueSemaphore.acquire()
+            #print('Safe to check queue')
+            await self.checkQueue()
+
+
+    def createThreads(self, playlist, sendGif):
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            results = [executor.submit(self.thrAddQueue(video, sendGif)) for video in playlist.videos]
+            trues = 0
+            for f in results:
+                result = f in concurrent.futures.as_completed(results)
+                if result is True:
+                    trues += 1
+
+        asyncio.run_coroutine_threadsafe(self.createThreadsCallback(trues, len(playlist)), bot.loop)
+
+    async def createThreadsCallback(self, addedSuccessfully, playlistLength):
+        await self.ctx.channel.send('>>> Finished. Added **{}** of **{}** songs to the queue.'
+                                    .format(addedSuccessfully, playlistLength))
+
+    def thrAddQueue(self, video, sendGif):
+        # Returns true if song was added, false otherwise
+        try:
+            filename_ = "".join(
+                [c for c in video.title if c.isalpha() or c.isdigit() or c == ' ']).rstrip()
+            filename_ = re.sub(' +', ' ', filename_)
+            filename_ += '.mp4'
+            print("Added {}".format(filename_))
+            self.queueLock.acquire()
+            self.queue.append(
+                {'filename': filename_, 'video': video, 'sendGif': sendGif})
+            self.queueLock.release()
+            self.queueSemaphore.release()
+            return True
+        except (pytube.exceptions.RegexMatchError or pytube.exceptions.VideoPrivate or
+                pytube.exceptions.VideoUnavailable):
+            # Video query was not possible
+            print("Error! Couldn't add {}".format(filename_))
+            return False
 
     async def checkQueue(self):
         if (self.source is not None) and (not self.player.is_playing()) and (not self.player.is_paused()):
@@ -104,10 +140,14 @@ class MusicPlayer:
             os.remove(current_)
             self.source = None
         try:
+            self.queueLock.acquire()
             self.current = self.queue.pop(0)
+            self.queueLock.release()
+            self.queueSemaphore.acquire()
             self.pauseStart = 0
             self.currentPauseTime = 0
         except IndexError:
+            self.queueLock.release()
             self.source = None
             return await self.destroy()
         await self.playAudio()
@@ -143,6 +183,7 @@ class MusicPlayer:
         queueString += '```Current: {} | ({} / {})\n\n'.format(video.title, self.getCurrentTimestamp(),
                                                                MusicPlayer.getVideoLength(video))
         i = 1
+        self.queueLock.acquire()
         for element in self.queue:
             video = element['video']
             toAdd = '{}: {} | ({})\n'.format(i, video.title, MusicPlayer.getVideoLength(video))
@@ -152,6 +193,7 @@ class MusicPlayer:
                 queueString += '> and {} more...'.format(len(self.queue) + 2 - i)
                 break
             queueString += toAdd
+        self.queueLock.release()
         queueString += '```'
         await self.ctx.message.reply(queueString)
 
@@ -185,7 +227,7 @@ class MusicPlayer:
     def shuffle(self):
         random.shuffle(self.queue)
 
-    # async def
+
 
     @staticmethod
     def getVideoLength(video):
@@ -274,8 +316,7 @@ async def on_command_error(ctx, error):
 
 
 
-class Utility(commands.Cog):
-
+class Testing(commands.Cog):
     # ---Commands---
     @commands.command(
         brief='Repeats the given query.',
@@ -284,116 +325,110 @@ class Utility(commands.Cog):
     async def echo(self, ctx, *, arg):
         await ctx.message.reply(arg)
 
+    @commands.command()
+    async def add(self, ctx, a: int, b: int):  # converts a and b to ints during invoke
+        if a == 9 and b == 10:
+            a = 11  # 9 + 10 = 21
+        await ctx.message.reply(a + b)
 
-@bot.command()
-async def add(ctx, a: int, b: int):  # converts a and b to ints during invoke
-    if a == 9 and b == 10:
-        a = 11  # 9 + 10 = 21
-    await ctx.message.reply(a + b)
+    @commands.command()
+    async def whereAmI(self, ctx):
+        if getUserVoiceChannel(ctx) is None:
+            return await ctx.message.reply('You are not in a voice channel currently.')
+        await ctx.message.reply(getUserVoiceChannel(ctx))
 
+class Music(commands.Cog):
+    @commands.command()
+    async def play(self, ctx, sendGif_):
+        await ctx.invoke(bot.get_command('join'))
+        global musicPlayer
+        if musicPlayer is None:
+            # Create musicPlayer if it doesn't exist
+            musicPlayer = MusicPlayer(ctx)
 
-@bot.command()
-async def whereAmI(ctx):
-    if getUserVoiceChannel(ctx) is None:
-        return await ctx.message.reply('You are not in a voice channel currently.')
-    await ctx.message.reply(getUserVoiceChannel(ctx))
+        if sendGif_ is not True:
+            sendGif_ = False
+            # sendGif_ is the message given by default, over-ridden with playi
 
+        musicPlayer.updateCtx(ctx)
+        await musicPlayer.addToQueue(sendGif_)
 
-@bot.command()
-async def join(ctx):
-    voiceChannel = getUserVoiceChannel(ctx)
-    if voiceChannel is None:
-        await ctx.message.reply('**Invalid command usage!** You must be connected to a voice channel.')
-        raise commands.CommandError('HANDLED')
-    if ctx.voice_client is not None:
-        return await ctx.voice_client.move_to(voiceChannel)
-    await voiceChannel.connect()
+    @commands.command()
+    async def playi(self, ctx):
+        await ctx.invoke(bot.get_command('play'), True)
 
+    @commands.command()
+    async def queue(self, ctx):
+        if musicPlayer is None:
+            await ctx.message.reply('The queue is empty.')
+            return
+        musicPlayer.updateCtx(ctx)
+        await musicPlayer.showQueue()
 
-@bot.command()
-async def leave(ctx):
-    if musicPlayer is None:
-        await ctx.voice_client.disconnect()
-    else:
-        await musicPlayer.destroy()
+    @commands.command()
+    async def pause(self, ctx):
+        if musicPlayer is None:
+            await ctx.message.reply('There is nothing to pause.')
+            return
+        musicPlayer.pause()
 
+    @commands.command()
+    async def resume(self, ctx):
+        if musicPlayer is None:
+            await ctx.message.reply('There is nothing to resume.')
+            return
+        musicPlayer.resume()
 
-@bot.command()
-async def play(ctx, sendGif_):
-    await ctx.invoke(bot.get_command('join'))
-    global musicPlayer
-    if musicPlayer is None:
-        # Create musicPlayer if it doesn't exist
-        musicPlayer = MusicPlayer(ctx)
+    @commands.command()
+    async def skip(self, ctx):
+        if musicPlayer is None:
+            await ctx.message.reply('There is nothing to skip.')
+            return
+        await musicPlayer.skip()
 
-    if sendGif_ is not True:
-        sendGif_ = False
-        # sendGif_ is the message given by default, over-ridden with playi
+    @commands.command()
+    async def shuffle(self, ctx):
+        if musicPlayer is None:
+            await ctx.message.reply('There is no queue.')
+            return
+        musicPlayer.shuffle()
 
-    musicPlayer.updateCtx(ctx)
-    await musicPlayer.addToQueue(sendGif_)
+class Utility(commands.Cog):
+    @commands.command()
+    async def join(self, ctx):
+        voiceChannel = getUserVoiceChannel(ctx)
+        if voiceChannel is None:
+            await ctx.message.reply('**Invalid command usage!** You must be connected to a voice channel.')
+            raise commands.CommandError('HANDLED')
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(voiceChannel)
+        await voiceChannel.connect()
 
+    @commands.command()
+    async def leave(self, ctx):
+        if musicPlayer is None:
+            await ctx.voice_client.disconnect()
+        else:
+            await musicPlayer.destroy()
 
-@bot.command()
-async def playi(ctx):
-    await ctx.invoke(bot.get_command('play'), True)
+class General(commands.Cog):
+    @commands.command()
+    async def randomMsg(self, ctx):
+        messages = await ctx.channel.history(limit=100).flatten()
+        selected = random.choice(messages)
+        await selected.reply('This message is from ---')
+        # TODO overhaul this
 
-
-@bot.command()
-async def queue(ctx):
-    if musicPlayer is None:
-        await ctx.message.reply('The queue is empty.')
-        return
-    musicPlayer.updateCtx(ctx)
-    await musicPlayer.showQueue()
-
-
-@bot.command()
-async def pause(ctx):
-    if musicPlayer is None:
-        await ctx.message.reply('There is nothing to pause.')
-        return
-    musicPlayer.pause()
-
-
-@bot.command()
-async def resume(ctx):
-    if musicPlayer is None:
-        await ctx.message.reply('There is nothing to resume.')
-        return
-    musicPlayer.resume()
-
-
-@bot.command()
-async def skip(ctx):
-    if musicPlayer is None:
-        await ctx.message.reply('There is nothing to skip.')
-        return
-    await musicPlayer.skip()
-
-
-@bot.command()
-async def shuffle(ctx):
-    if musicPlayer is None:
-        await ctx.message.reply('There is no queue.')
-        return
-    musicPlayer.shuffle()
-
-
-@bot.command()
-async def randomMsg(ctx):
-    messages = await ctx.channel.history(limit=100).flatten()
-    selected = random.choice(messages)
-    await selected.reply('This message is from ---')
-    # TODO overhaul this
+    @commands.command()
+    async def fox(self, ctx):
+        response = requests.get('https://randomfox.ca/floof').json()
+        await ctx.message.reply(response.get('image'))
 
 
-@bot.command()
-async def fox(ctx):
-    response = requests.get('https://randomfox.ca/floof').json()
-    await ctx.message.reply(response.get('image'))
 
-
+bot.add_cog(Testing())
+bot.add_cog(Music())
 bot.add_cog(Utility())
+bot.add_cog(General())
 bot.run(os.getenv('DISCORD_TOKEN'))
 
